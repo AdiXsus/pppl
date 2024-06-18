77678814130ea2dd1fa454c3661fbcb14f2e7bfe
AdiXsus <58894271+AdiXsus@users.noreply.github.com>
2024-06-19 00:29:44 +0200

    Update config.js

--- config/config.js
+++ config/config.js
@@ -3,7 +3,7 @@ module.exports = {
     token: "", 
 
     // ID serwera Discord
-    guildId: "717165904957276282", // ID Twojego Serwera Discord
+    guildId: "901923526028693554", // ID Twojego Serwera Discord
 
     // Nazwa Twojego Serwera
     Nazwa: "",
@@ -12,19 +12,19 @@ module.exports = {
     głównykolor: "#ffec00", // Hex głównego koloru Serwera
 
    // Wezwania Poczekalnia (Embed jeśli ktoś dołączy na kanał z podanym niżej ID)
-   kanalpoczekalniawezwanie: "1252708006026674287", // ID kanału VOICE (Głosowego) - (np. Poczekalnia do Administracji)
-   kanalpowiadomienwezwanie: "1252707942411538506", // ID kanału TXT (Tekstowego) - (Jeśli ktoś dołączy już na ten kanał VOICE to na te ID kanału bot wyśle EMBED)
-   rolawezwania: ["1252261092948709489", "1252261092948709489"], // ID rangi jaką ma oznaczyć nad tą wiadomością (np. Rola Administracji)
+   kanalpoczekalniawezwanie: "1080488361787129867", // ID kanału VOICE (Głosowego) - (np. Poczekalnia do Administracji)
+   kanalpowiadomienwezwanie: "1252708352996151296", // ID kanału TXT (Tekstowego) - (Jeśli ktoś dołączy już na ten kanał VOICE to na te ID kanału bot wyśle EMBED)
+   rolawezwania: ["1119240101059375126", "1119239800684290118", "1119239914836459550"], // ID rangi jaką ma oznaczyć nad tą wiadomością (np. Rola Administracji)
 
    // Whitelist
-   okienkorolaoznaczenie: "1252268813421121536", // Jaką rangę ma oznaczać jeśli ktoś pyta WL
-   INFO_CHANNEL_OKIENKO: "1252707658042183772", // Kanał gdzie ma się wysyłać ogłoszenie że ktoś pyta WL
+   okienkorolaoznaczenie: "1251871534348238848", // Jaką rangę ma oznaczać jeśli ktoś pyta WL
+   INFO_CHANNEL_OKIENKO: "1230086906335723532", // Kanał gdzie ma się wysyłać ogłoszenie że ktoś pyta WL
    LINK_DO_REGULAMINU_SERWERA: "https://discord.com/channels/901923526028693554/1080488352773570580",
    color_okienko: "a845a1",
    //                Whitelist - COMMANDS                       //
-   ID_KANAŁU_ZDANE_WL: "1252707699943276615", // Ranaga Zdane Whitelist
-   ID_KANAŁU_NIE_ZDANE_WL: "1252707744537116753", // Ranga nie zdane Whitelist
-   WL_ZDANE_RANGA: "734324021558247535", // Jaką rangę ma dodawać jeśli ktoś zda WL (ID rangi)
-   ID_KANALU_GLOSOWEGO_POCZEKALNI: "1252707779202912356", // ID kanału Poczekalni WL (VOICE - Głosowy)
+   ID_KANAŁU_ZDANE_WL: "1192579353448558763", // Ranaga Zdane Whitelist
+   ID_KANAŁU_NIE_ZDANE_WL: "1192579426437844992", // Ranga nie zdane Whitelist
+   WL_ZDANE_RANGA: "1192579506821664838", // Jaką rangę ma dodawać jeśli ktoś zda WL (ID rangi)
+   ID_KANALU_GLOSOWEGO_POCZEKALNI: "1215348334861094993", // ID kanału Poczekalni WL (VOICE - Głosowy)
 
 }
