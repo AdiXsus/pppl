ac043948dafdf7337dbf50bc0de3636c708f03c8
AdiXsus <58894271+AdiXsus@users.noreply.github.com>
2024-06-19 12:16:49 +0200

    Update index.js

--- index.js
+++ index.js
@@ -31,66 +31,6 @@ client.on('ready', () => {
     // Ustawienie customowego statusu
 });
 
-// invite-tracker
-
-const InvitesTracker = require("@androz2091/discord-invites-tracker");
-const tracker = InvitesTracker.init(client, {
-    fetchGuilds: true,
-    fetchVanity: true,
-    fetchAuditLogs: true,
-});
-
-tracker.on("guildMemberAdd", (member, type, invite) => {
-    const logChannel = member.guild.channels.cache.get(
-        client.config.inviteLoggerChannel,
-    );
-
-    const embed = new EmbedBuilder();
-    embed.setAuthor({
-        name: member.user.tag,
-        iconURL: member.user.displayAvatarURL({ dynamic: true }),
-    });
-    embed.setColor("#1800ff");
-    embed.setThumbnail(member.user.displayAvatarURL({ dynamic: true }));
-    embed.setFooter({
-        text: `Obecna ilość użtkowników: ${member.guild.memberCount}`,
-    });
-
-    if (type === "normal") {
-        embed.setDescription(
-            `Użytownik <@${member.id}> | ${member.user.tag} | ${member.user.id} został zaproszony przez <@${invite.inviter.id}> | ${invite.inviter.tag}`,
-        );
-        logChannel.send({ embeds: [embed] });
-    } else if (type === "vanity") {
-        embed.setDescription(
-            `Użytownik <@${member.id}> | ${member.user.tag} | ${member.user.id} dołączył używając niestandardowego zaproszenia!`,
-        );
-        logChannel.send({ embeds: [embed] });
-    } else if (type === "permissions") {
-        embed.setDescription(
-            `Użytownik <@${member.id}> | ${member.user.tag} | ${member.user.id} dołączył na serwer, lecz nie mam uprawnień do wyświetlenia przez kogo!`,
-        );
-        logChannel.send({ embeds: [embed] });
-    } else if (type === "unknown") {
-        embed.setDescription(
-            `Użytownik <@${member.id}> | ${member.user.tag} | ${member.user.id} dołączył na serwer!`,
-        );
-        logChannel.send({ embeds: [embed] });
-    }
-});
-
-const { GiveawaysManager } = require("discord-giveaways");
-const manager = new GiveawaysManager(client, {
-    storage: "./giveaways.json",
-    default: {
-        botsCanWin: false,
-        embedColor: "#FF0000",
-        embedColorEnd: "#000000",
-        reaction: "🎉",
-    },
-});
-
-client.giveawaysManager = manager;
 
 client.commands = new Collection();
 
